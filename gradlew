3bb06f0940a1ceea79d1b38c6283976cfe7d7a54
BaysSH <vitaliybays94@gmail.com>
2025-08-26 22:14:13 +0300

    Добавил Gradle Wrapper

--- gradlew
+++ gradlew
@@ -1,21 +1,7 @@
-﻿#!/usr/bin/env sh
+#!/usr/bin/env sh
 
-##############################################################################
-##
-##  Gradle start up script for UN*X
-##
-##############################################################################
+DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-APP_NAME="Gradle"
-APP_BASE_NAME=`basename "$0"`
+CLASSPATH="$DIR/gradle/wrapper/gradle-wrapper.jar"
 
-# Add default JVM options here if desired
-DEFAULT_JVM_OPTS=""
-
-DIRNAME=`dirname "$0"`
-if [ "$DIRNAME" = "." ]; then
-  DIRNAME=`pwd`
-fi
-CLASSPATH=$DIRNAME/gradle/wrapper/gradle-wrapper.jar
-
-exec "$JAVA_HOME/bin/java" $DEFAULT_JVM_OPTS -cp "$CLASSPATH" org.gradle.wrapper.GradleWrapperMain "$@"
+exec java -cp "$CLASSPATH" org.gradle.wrapper.GradleWrapperMain "$@"
--- gradlew.bat
+++ gradlew.bat
@@ -1,16 +1,17 @@
-﻿@echo off
-@rem ##########################################################################
-@rem
-@rem  Gradle startup script for Windows
-@rem
-@rem ##########################################################################
+@ECHO OFF
+SETLOCAL
 
-set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
+set DIR=%~dp0
+IF EXIST "%DIR%\gradle\wrapper\gradle-wrapper.jar" (
+    set CLASSPATH=%DIR%\gradle\wrapper\gradle-wrapper.jar
+) ELSE (
+    echo Gradle wrapper JAR not found. Exiting.
+    exit /b 1
+)
+
 set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
+set APP_HOME=%DIR%
+
 set DEFAULT_JVM_OPTS=
 
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
-"%JAVA_HOME%\bin\java.exe" %DEFAULT_JVM_OPTS% -cp "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+java %DEFAULT_JVM_OPTS% -cp "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
